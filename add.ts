79dbde90dae130f4501455449e4dd02d81819744
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 14:55:43 +0900

    Update add.ts

--- add.ts
+++ add.ts
@@ -2,22 +2,22 @@ class complex{
 real: number
 imag: number
     constructor(real: number,imag: number) {
-    this.real = real
-    this.imag = imag
+    this.real = real;
+    this.imag = imag;
     }
 }
-let x: any
-let y: any
+let x: any;
+let y: any;
 function toString(x: complex){
-const z = x.real.toString() + '+' + x.imag.toString() + 'i'
-    return z
+const z = x.real.toString() + '+' + x.imag.toString() + 'i';
+    return z;
 }
 function add(x: complex,y: complex){
-var real = x.real + y.real
-var imag = x.imag + y.imag
-const z = real.toString() + '+' + imag.toString() + 'i'
-return z
+var real = x.real + y.real;
+var imag = x.imag + y.imag;
+const z = real.toString() + '+' + imag.toString() + 'i';
+return z;
 }
-const a = new complex(1,2)
-const b = new complex(3,4)
-console.log(add(a,b))
+const a = new complex(1,2);
+const b = new complex(3,4);
+console.log(add(a,b));
