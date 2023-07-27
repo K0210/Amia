0b4eae5fd25813bdb4d0751e63aad6e5b1b04fcb
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 14:44:59 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,17 @@
 class complex{
-  real: number
-  imag: number
-    constructor(real: number, imag:number) {
-    this.imag = real
+real: number
+imag: number
+    constructor(real: number,imag: number) {
+    this.real = real
     this.imag = imag
     }
 }
 const a = new complex(1,2)
 const b = new complex(3,4)
-var real = a.real + b.real
-var imag = a.imag + b.imag
-const c = real.toString() + '+' + imag.toString() + 'i'
-console.log(c)
+function add(x,y){
+var real = x.real + y.real
+var imag = x.imag + y.imag
+const z = real.toString() + '+' + imag.toString() + 'i'
+return z
+}
+console.log(add(a,b))
