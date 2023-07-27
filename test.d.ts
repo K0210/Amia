4ebc44a565e8a8e4dc2e6f1b7f7e14153218e90f
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 17:53:09 +0900

    Create test.d.ts

---
+++ test.d.ts
@@ -0,0 +1,15 @@
+declare class complex {
+    real: number;
+    imag: number;
+    constructor(real: number, imag: number);
+    abs(): number;
+    abs2(): number;
+}
+declare let x: any;
+declare let y: any;
+declare function add(x: complex, y: complex): complex;
+declare function sub(x: complex, y: complex): complex;
+declare function mul(x: complex, y: complex): complex;
+declare function div(x: complex, y: complex): complex;
+declare const a: complex;
+declare let c: number;
