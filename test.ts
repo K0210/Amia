b9160ccf2425b05db9a10606fb783705ef2d9c6f
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 14:40:57 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,14 @@
-function amia(x: number){
-let a = x;
-  a = x + ''
-return a
+class complex{
+  real: number
+  imag: number
+    constructor(real: number, imag:number) {
+    this.imag = real
+    this.imag = imag
+    }
 }
+const a = new complex(1,2)
+const b = new complex(3,4)
+var real = a.real + b.real
+var imag = a.imag + b.imag
+const c = real.toString() + '+' + imag.toString() + 'i'
+console.log(c)
