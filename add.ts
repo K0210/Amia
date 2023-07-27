7e844c10e31d643840b73d86ffc167693690ebe5
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 14:47:24 +0900

    Create add.ts

---
+++ add.ts
@@ -0,0 +1,19 @@
+class complex{
+real: number
+imag: number
+    constructor(real: number,imag: number) {
+    this.real = real
+    this.imag = imag
+    }
+}
+const a = new complex(1,2)
+const b = new complex(3,4)
+let x: any
+let y: any
+function add(x: complex,y: complex){
+var real = x.real + y.real
+var imag = x.imag + y.imag
+const z = real.toString() + '+' + imag.toString() + 'i'
+return z
+}
+console.log(add(a,b))
