4aea15414051fd66722b6ccd845226ae3ce2015d
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-27 14:52:07 +0900

    Update add.ts

--- add.ts
+++ add.ts
@@ -10,6 +10,10 @@ const a = new complex(1,2)
 const b = new complex(3,4)
 let x: any
 let y: any
+function toString(x: complex){
+const z = x.real.toString() + '+' + x.imag.toString() + 'i'
+    return z
+}
 function add(x: complex,y: complex){
 var real = x.real + y.real
 var imag = x.imag + y.imag
