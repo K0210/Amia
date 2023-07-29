83cbe3286a50517d0a48e9ba84d06ff8b2f466c9
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-30 00:27:56 +0900

    Update Amia_dual.py

--- Amia_dual.py
+++ Amia_dual.py
@@ -12,21 +12,34 @@ class dual_number(object):
     imag = self.imag + other.imag
     z = dual_number(real, imag)
     return z
+  def __iadd__(self, other):
+    self.real = self.real + other.real
+    self.imag = self.imag + other.imag
   def __sub__(self, other):
     real = self.real - other.real
     imag = self.imag - other.imag
     z = dual_number(real, imag)
     return z
+  def __isub__(self, other):
+    self.real = self.real - other.real
+    self.imag = self.imag - other.imag
   def __mul__(self, other):
     real = self.real * other.real
     imag = self.real * other.imag + self.imag * other.real
     z = dual_number(real, imag)
     return z
+  def __imul__(self, other):
+    self.real = self.real * other.real
+    self.imag = self.real * other.imag + self.imag * other.real
   def __div__(self, other):
     k = other.con()
     amia = dual_number(other.real * other.real, 0)
     z = self * mul(k,amia)
     return z
+  def __idiv__(self, other):
+    k = other.con()
+    amia = dual_number(other.real * other.real, 0)
+    self = self * mul(k,amia
   def toString(self):
     if self.imag >= 0:
         z = str(self.real) + '+' + str(self.imag) + 'ε'
