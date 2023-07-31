70408ff3f8b643faaec7266a4974e32c924f0f36
K0210 <119788148+K0210@users.noreply.github.com>
2023-07-31 18:44:26 +0900

    Create Amia_dual_patch.ts

---
+++ TypeScript/Amia_dual/Amia_dual_patch.ts
@@ -0,0 +1,31 @@
+import "./Amia_dual"
+function cos(x: dual_number){
+  const real = Math.cos(x.real);
+  const imag = -x.imag * Math.sin(x.real);
+  const z = new complex(real, imag);
+  return z;
+}
+function sin(x: dual_number){
+  const real = Math.sin(x.real);
+  const imag = x.imag * Math.cos(x.real);
+  const z = new complex(real, imag);
+  return z;
+}
+function tan(x: dual_number){
+  const real = Math.tan(x.real);
+  const imag = x.imag / (Math.cos(x.real) * Math.cos(x.real);
+  const z = new complex(real, imag);
+  return z;
+}
+function exp(x: dual_number){
+  const real = Math.exp(x.real);
+  const imag = x.imag * Math.exp(x.real);
+  const z = new complex(real, imag);
+  return z;
+}
+function log(x: dual_number, base: number = Math.E){
+  const real = Math.log(x.real,　base);
+  const imag = (x.imag / x.real) / Math.log(base);
+  const z = new complex(real, imag);
+  return z;
+}  
