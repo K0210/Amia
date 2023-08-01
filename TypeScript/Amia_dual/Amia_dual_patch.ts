486452554a49ffa381dbaeebea591110b3651d3a
K0210 <119788148+K0210@users.noreply.github.com>
2023-08-01 15:31:38 +0900

    Update Amia_dual_patch.ts

--- TypeScript/Amia_dual/Amia_dual_patch.ts
+++ TypeScript/Amia_dual/Amia_dual_patch.ts
@@ -1,31 +1,31 @@
 import {dual_number} from "./Amia_dual"
 let x: any;
 let y: any; 
-function cos(x: dual_number){
+export function cos(x: dual_number){
   const real = Math.cos(x.real);
   const imag = -x.imag * Math.sin(x.real);
   const z = new dual_number(real, imag);
   return z;
 }
-function sin(x: dual_number){
+export function sin(x: dual_number){
   const real = Math.sin(x.real);
   const imag = x.imag * Math.cos(x.real);
   const z = new complex(real, imag);
   return z;
 }
-function tan(x: dual_number){
+export function tan(x: dual_number){
   const real = Math.tan(x.real);
   const imag = x.imag / (Math.cos(x.real) * Math.cos(x.real);
   const z = new complex(real, imag);
   return z;
 }
-function exp(x: dual_number){
+export function exp(x: dual_number){
   const real = Math.exp(x.real);
   const imag = x.imag * Math.exp(x.real);
   const z = new complex(real, imag);
   return z;
 }
-function log(x: dual_number, base: number = Math.E){
+export function log(x: dual_number, base: number = Math.E){
   const real = Math.log(x.real,　base);
   const imag = (x.imag / x.real) / Math.log(base);
   const z = new complex(real, imag);
